bf28349b6c37e730d093f83fe38e28bd742bb442
StonAi <372516527@qq.com>
2017-11-16 17:46:49 +0800

    提交

---
+++ STColorMake/STColorMake.h
@@ -0,0 +1,13 @@
+//
+//  STColorMake.h
+//  STRacDemo
+//
+//  Created by 石函东 on 2017/11/16.
+//  Copyright © 2017年 石函东. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface STColorMake : NSObject
++(UIColor *)getColor:(NSString*)hexColor;
+@end
---
+++ STColorMake/STColorMake.m
@@ -0,0 +1,29 @@
+//
+//  STColorMake.m
+//  STRacDemo
+//
+//  Created by 石函东 on 2017/11/16.
+//  Copyright © 2017年 石函东. All rights reserved.
+//
+
+#import "STColorMake.h"
+
+@implementation STColorMake
++(UIColor *)getColor:(NSString*)hexColor
+{
+    unsigned int red,green,blue;
+    NSRange range;
+    range.length = 2;
+    
+    range.location = 0;
+    [[NSScanner scannerWithString:[hexColor substringWithRange:range]]scanHexInt:&red];
+    
+    range.location = 2;
+    [[NSScanner scannerWithString:[hexColor substringWithRange:range]]scanHexInt:&green];
+    
+    range.location = 4;
+    [[NSScanner scannerWithString:[hexColor substringWithRange:range]]scanHexInt:&blue];
+    
+    return [UIColor colorWithRed:(float)(red/255.0f)green:(float)(green / 255.0f) blue:(float)(blue / 255.0f)alpha:1.0f];
+}
+@end
